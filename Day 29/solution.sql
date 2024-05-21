1f1646e853c9285cf7b36c044efbf23e6827af23
Shreyajyoti Dutta <53636141+HeatTransfer@users.noreply.github.com>
2024-05-21 21:52:47 +0530

    Update solution.sql

--- Day 29/solution.sql
+++ Day 29/solution.sql
@@ -1,15 +1,6 @@
-WITH cte AS
-(SELECT Team1,
-	CASE WHEN Winner = Team1 THEN 1 ELSE 0 END AS points
-FROM cricket_matches
-UNION ALL
-SELECT Team2,
-	CASE WHEN Winner = Team2 THEN 1 ELSE 0 END AS points
-FROM cricket_matches)
-
-SELECT Team1,
-	COUNT(points) AS total_matches_played,
-	SUM(CASE WHEN points = 1 THEN 1 ELSE 0 END) AS wins,
-    SUM(CASE WHEN points = 0 THEN 1 ELSE 0 END) AS losses
-FROM cte
-GROUP BY Team1;
+DELETE p1.*
+FROM Person p1
+CROSS JOIN Person p2
+WHERE p1.email = p2.email
+AND 
+p1.id > p2.id;
